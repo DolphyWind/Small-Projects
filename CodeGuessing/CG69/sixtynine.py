b30c2a1fd5eaf3621b2db1348fa2f0e3efb4f6fe
DolphyWind <yunusaydin590@gmail.com>
2025-02-13 17:58:00 +0300

    Add CG69

---
+++ CodeGuessing/CG69/sixtynine.py
@@ -0,0 +1,15 @@
+from math import *
+
+# security :D
+x = eval(input("Target number: "))
+
+print(f"x + 69 = {x + 69}")
+print(f"x - 69 = {x - 69}")
+print(f"x / 69 = {x / 69}")
+print(f"x * 69 = {x * 69}")
+print(f"x^69 = {x ** 69}")
+print(f"x^(1 / 69) = {x ** (1 / 69)}")
+if x != 0:
+    print(f"69 / x = {69 / x}")
+if x > 0:
+    print(f"log_69(x) = {log(x, 69)}")
